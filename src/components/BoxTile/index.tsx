b6c665a8b2d7e50729a64fbee7d741b473b17b0e
Redeemer <red.pace.dev@gmail.com>
2022-11-18 01:58:55 +0100

    feat: Added BoxTile to be used as general component for various of places

---
+++ src/components/BoxTile/index.tsx
@@ -0,0 +1,55 @@
+import { FC } from "react";
+
+import tw, { styled } from "twin.macro";
+
+interface BoxTileProps {
+  title: string;
+  subtitle?: string;
+  activeSubtitle?: boolean;
+  icon?: string;
+  description?: string;
+  isFullBorder?: boolean;
+  withRandomBorder?: boolean;
+  isFullWidth?: boolean;
+}
+
+interface ItemContentProps {
+  withRandomBorder?: boolean;
+  isFullBorder?: boolean;
+  isFullWidth?: boolean;
+}
+
+interface SubtitleProps {
+  active?: boolean;
+}
+
+const Item = styled.div(({ withRandomBorder, isFullBorder, isFullWidth }: ItemContentProps) => [
+  tw`w-full m-3 p-[20px] text-base bg-[#101010] rounded-lg border-[#4bffa5] border-dotted
+  hover:animate-[border-transition 1s ease-out 0s infinite]
+  `,
+  !withRandomBorder && tw`border-l-[1px] border-b-[1px]`,
+  withRandomBorder && tw`border-r-[1px] border-t-[1px]`,
+  isFullBorder && tw`border-[0px] border-b-[1px]`,
+  !isFullWidth && tw`lg:w-[45%]`
+]);
+
+const Icon = tw.i`text-xl text-[#4bffa5] font-normal text-center w-full`;
+
+const ItemTitle = tw.div`text-lg m-[15px 0] text-[#eee] font-semibold text-center w-full`;
+
+const ItemDescription = tw.div`text-[#c3c3c3] text-justify`;
+
+const ItemSubtitle = styled.div(({ active = false }: SubtitleProps) => [
+  tw`relative m-[0 0 5px 0] inline-block text-xs text-[#999]`,
+  active && tw`text-[#4bffa5] font-medium`
+]);
+
+
+export const BoxTile: FC<BoxTileProps> = ({ isFullBorder, withRandomBorder, isFullWidth, title, subtitle, activeSubtitle, description, icon }: BoxTileProps) => (
+  <Item withRandomBorder={withRandomBorder} isFullBorder={isFullBorder} isFullWidth={isFullWidth}>
+    { subtitle && <ItemSubtitle active={activeSubtitle}>{ subtitle }</ItemSubtitle> }
+    { icon && <Icon className={icon} /> }
+    <ItemTitle>{ title }</ItemTitle>
+    <ItemDescription>{ description }</ItemDescription>
+  </Item>
+);

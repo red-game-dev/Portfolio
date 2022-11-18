4e60881d4e2cdc8da66c82eb9e9b42c05b314fea
Redeemer <red.pace.dev@gmail.com>
2022-11-18 01:59:14 +0100

    feat: Added cover component which will be used on top of the page

---
+++ src/components/Cover/index.tsx
@@ -0,0 +1,54 @@
+import tw, { styled } from "twin.macro";
+
+import Link from "next/link";
+
+import TypingAnimation from "@/components/TypingAnimation";
+
+const Introduction = styled.h1(() => [
+  tw`m-0 absolute text-white break-words left-0 pr-12 text-base 
+    bottom-[30px]
+    lg:text-lg lg:left-[calc(20% + 35px)] lg:bottom-[50px] lg:max-w-[560px]
+    [& > strong]:text-[#4bffa5]`
+]);
+
+const Section = tw.div`relative overflow-hidden h-screen m-0 py-0 px-[30px] lg:p-0 z-[7] `;
+
+const Content = tw.div`relative top-0 left-0 z-[2] table table w-full h-full align-middle`;
+
+const TitleWrapper = tw.div`relative top-0 left-0 z-[2] table-fixed table-cell w-full h-full align-middle`;
+
+const ScrollerLink = styled(Link)(() => [
+  tw`absolute w-5 h-5 left-[47.4%] lg:left-[49.3%] z-[2] text-[#4bffa5] right-auto top-auto text-2xl text-center 
+  animate-[mouse-anim-mobile 1s ease-out 0s infinite]
+  lg:animate-[mouse-anim-desktop 1s ease-out 0s infinite]
+  `
+]);
+
+const ScrollerIcon = tw.i`relative bottom-[10px]`;
+
+const VideoContainer = tw.div`ml-0 left-0 absolute top-0 h-full bg-no-repeat bg-center bg-cover lg:left-1/2 lg:ml-[-50vw] lg:w-screen`;
+
+const VideoBackgroundMask = tw.div`absolute top-0 left-0 w-full h-full opacity-40 z-[2] bg-[#101010]`;
+
+const VideoBackgroundTexture = tw.div`absolute top-0 left-0 w-full h-full z-[2]`;
+
+export const Cover = ({ intro, image = "https://launcher.goz.fun/resources/images/chapter-5-discover-of-new-lands-reverse-top-logos.jpg" }: any) => (
+    <Section id="section-started">
+      <VideoContainer
+        className="jarallax"
+        style={{ backgroundImage: `url(${image})` }}
+      >
+        <VideoBackgroundMask />
+        <VideoBackgroundTexture id="grained_container" />
+      </VideoContainer>
+      <Content>
+          <Introduction dangerouslySetInnerHTML={{ __html: intro }} />
+          <TitleWrapper>
+            <TypingAnimation />
+          </TitleWrapper>
+      </Content>
+      <ScrollerLink href="#section-about">
+        <ScrollerIcon className="icon fas fa-chevron-down" />
+      </ScrollerLink>
+    </Section>
+  );

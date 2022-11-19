48788302c50d0fc45b42cbd61164f4d636f03126
Redeemer <red.pace.dev@gmail.com>
2022-11-19 08:59:10 +0100

    fix: Fixed modal appearing not at the right time

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,4 +1,4 @@
-import tw, { styled } from "twin.macro";
+import tw, { css, styled } from "twin.macro";
 
 import { useModalStateHook } from "@/components/Modal/hooks/useModalStateHook";
 import { ProjectModal } from "@/components/Modal/ProjectModal";
@@ -13,13 +13,18 @@ interface PopupBoxProps {
 const PopupBox = styled.div(({ isVisible }: PopupBoxProps) => [
   tw`fixed top-0 right-0 bottom-0 right-0 w-full h-full bg-[#101010] overflow-auto`,
   !isVisible && tw`
+  invisible
   absolute
-  bg-red-100
   rounded-full w-1/2 h-1/2
   animate-bounce
   transition-[5s all linear]
   delay-1000 z-[-10]`,
-  isVisible && tw`z-[9999] animate-[bounceIn 900ms ease-in-out 0s 1]`,
+  isVisible && tw`visible z-[9999] animate-[bounceIn 900ms ease-in-out 0s 1]`,
+  css`
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  `
 ]);
 
 export const Modal = () => {

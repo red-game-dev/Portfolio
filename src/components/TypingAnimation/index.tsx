dbadc7979cfbfefece5757c896549ba3e0566586
Redeemer <red.pace.dev@gmail.com>
2022-11-21 19:43:04 +0100

    feat: Added small animation upon loading the title

--- src/components/TypingAnimation/index.tsx
+++ src/components/TypingAnimation/index.tsx
@@ -1,12 +1,13 @@
 import { FC } from "react";
 
-import tw from "twin.macro";
+import tw, { styled } from "twin.macro";
 
 import { useType } from "@/components/TypingAnimation/hooks/useType";
 
-const Subtitle = tw.h1`
-  text-white text-6xl w-full m-auto text-center
-  [& > span]:text-[#4bffa5] [& > span]:font-bold`;
+const Subtitle = styled.h1(() => [
+  tw`text-white text-6xl w-full m-auto text-center
+     [& > span]:text-[#4bffa5] [& > span]:font-bold animate-[move-text 0.75s forwards, text-color 0.75s forwards, border-transition 1s ease-in-out 0s]`
+]);
 
 interface TypingAnimationProps {
   typingData: string[];
@@ -16,14 +17,12 @@ const TypingAnimation: FC<TypingAnimationProps> = ({ typingData }: TypingAnimati
   const currentLetters = useType(typingData);
 
   return (
-    <>
-      <Subtitle
-        id="typing-title"
-        dangerouslySetInnerHTML={{
-          __html: currentLetters
-        }}
-      />
-    </>
+    <Subtitle
+      id="typing-title"
+      dangerouslySetInnerHTML={{
+        __html: currentLetters
+      }}
+    />
   );
 };
 

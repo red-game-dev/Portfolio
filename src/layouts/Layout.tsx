dddf0ef41e85fb534df6d99706dead9d76b07078
Redeemer <red.pace.dev@gmail.com>
2022-11-24 21:22:56 +0100

    fix: Fixed lint issues

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -3,12 +3,12 @@ import { FC, Fragment } from "react";
 import tw from "twin.macro";
 
 import { AppLoader, AppLoadingLines } from "@/components/AppLoader";
+import { useAppLoaderStateHook } from "@/components/AppLoader/hooks/useAppLoaderStateHook";
 import { Modal } from "@/components/Modal";
 import { portfolioData  } from "@/data/resume";
 
 import Footer from "./Footer";
 import Header from "./Header";
-import { useAppLoaderStateHook } from "@/components/AppLoader/hooks/useAppLoaderStateHook";
 
 const Container = tw.div`relative m-0 overflow-hidden before:z-[8] before:pointer-events-none`;
 
@@ -26,7 +26,7 @@ const Layout: FC<LayoutProps> = ({ title, children }: LayoutProps) => {
     <>
       <Modal />
       <AppLoader />
-      <Container style={isLoading ? { display: 'none'} : {}}>
+      <Container style={isLoading ? { display: "none"} : {}}>
         <Header title={title} />
           <Wrapper>
             {children}
@@ -35,7 +35,7 @@ const Layout: FC<LayoutProps> = ({ title, children }: LayoutProps) => {
         <AppLoadingLines />
       </Container>
     </>
-  )
+  );
 };
 
 export default Layout;

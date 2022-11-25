257719e7692ba04a87988803c8fa0e373bcd0d93
Redeemer <red.pace.dev@gmail.com>
2022-11-25 04:04:46 +0100

    fix: Fixed list does not contain only li

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -14,11 +14,11 @@ const FooterContainer = styled.footer(() => [
   tw`p-0 fixed bottom-0 left-12 right-12 w-auto h-1 z-[10] hidden lg:block`
 ]);
 
-const SocialMediaList = styled.ul(() => [
+const SocialMediaList = styled.div(() => [
   tw`float-right absolute right-0 bottom-0 w-6 list-none last:mb-0`
 ]);
 
-const FollowMeButton = styled.li(() => [
+const FollowMeButton = styled.span(() => [
   tw`
     pr-20 absolute bottom-full right-[5px] text-[#bbb] text-sm leading-6 h-6 whitespace-nowrap origin-[100% 0] text-right rotate-90
     after:content[''] after:absolute after:top-1/2 after:right-0 after:w-12 after:h-1 after:bg-[#4bffa5]
